304a700d927f2ffefac1a054e0122bf77cf5f6f4
aisayo <aisayo@icloud.com>
2017-10-25 02:30:05 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :category, inclusion: (%w(fiction, nonfiction))
+  validates :category, inclusion: ( %w(fiction, nonfiction ))
   validates :content, length: { minimum: 100 }
 end
