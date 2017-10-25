b1b3488455108055c13852cea9b3ad7f25932d2d
aisayo <aisayo@icloud.com>
2017-10-25 02:33:46 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,9 @@ class PostsController < ApplicationController
     if @post.valid?
       @post.save
     redirect_to post_path(@post)
-  else
+    else
       render :edit
-    
+    end
   end
 
   private
